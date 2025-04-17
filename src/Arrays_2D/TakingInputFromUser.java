7e50233cb0d995f223b6e97e1fc1147edfc4f8c3
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-17 18:44:01 +0530

    Source Code

--- src/Arrays_2D/TakingInputFromUser.java
+++ src/Arrays_2D/TakingInputFromUser.java
@@ -8,7 +8,7 @@ public class TakingInputFromUser {
             for(int j=0; j<arr[i].length; j++){ // column
                 System.out.print(arr[i][j]+"  ");
             }
-            System.out.println();
+            git stautsSystem.out.println();
         }
     }
     public static void main(String[] args) {
