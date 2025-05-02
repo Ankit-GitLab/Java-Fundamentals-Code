b524428266f11726a8cf1247e14b2ce69eaa7754
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-02 18:38:57 +0530

    Source Code

--- src/ControlStatement_Math_String/Loop/For_Loop/PrintCount.java
+++ src/ControlStatement_Math_String/Loop/For_Loop/PrintCount.java
@@ -1,18 +1,26 @@
 package ControlStatement_Math_String.Loop.For_Loop;
+
 import java.util.*;
+
 public class PrintCount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Welcome message
         System.out.println("Welcome to print number");
+
+        // Prompt user to enter a number
         System.out.print("Please enter the number: ");
         int num = sc.nextInt();
-        count(num);
 
+        // Call method to print numbers from 1 to num
+        count(num);
     }
 
-    public static void count(int num){
-        for(int i=1; i<=num; i++){
-            System.out.print(i+" ");
+    // Method to print numbers from 1 to num using for loop
+    public static void count(int num) {
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i + " "); // Print number with space
         }
     }
 }
