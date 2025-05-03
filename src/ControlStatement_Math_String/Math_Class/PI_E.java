49554b5722151358ac0c937965d3f36fdbfcb7d5
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-04 00:24:13 +0530

    Source Code

---
+++ src/ControlStatement_Math_String/Math_Class/PI_E.java
@@ -0,0 +1,9 @@
+package ControlStatement_Math_String.Math_Class;
+
+public class PI_E {
+    public static void main(String[] args) {
+        // Accessing Math.PI and Math.E
+        System.out.println("Value of Pi: " + Math.PI);
+        System.out.println("Value of E: " + Math.E);
+    }
+}
---
+++ src/ControlStatement_Math_String/Math_Class/Sin.java
@@ -0,0 +1,12 @@
+package ControlStatement_Math_String.Math_Class;
+
+public class Sin {
+    public static void main(String[] args) {
+        double radians = Math.PI / 2; // 90 degrees in radians
+
+        // Using Math.sin()
+        double result = Math.sin(radians);
+
+        System.out.println("sin(90°) = " + result); // Output: 1.0
+    }
+}
