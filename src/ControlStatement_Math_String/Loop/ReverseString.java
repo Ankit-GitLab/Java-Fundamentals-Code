8b5391174f3147a04f974c24c060f16882fb713b
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-07 09:17:03 +0530

    Source Code

---
+++ src/ControlStatement_Math_String/Loop/ReverseString.java
@@ -0,0 +1,20 @@
+package ControlStatement_Math_String.Loop;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String original = "Ankit";
+        String reversed = "";
+
+        int i = original.length() - 1; // Start from last character
+
+        // While loop to reverse string
+        while (i >= 0) {
+            reversed = reversed + original.charAt(i); // Add character from end
+            i--; // Move backward
+        }
+
+        System.out.println("Reversed String: " + reversed);
+    }
+}
+
