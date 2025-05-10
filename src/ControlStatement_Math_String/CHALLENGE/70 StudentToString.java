350beb298b7ad829a43ff97d3e312a9988ec0711
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-10 16:31:03 +0530

    Source code

--- src/ControlStatement_Math_String/CHALLENGE/70 StudentToString.java
+++ src/ControlStatement_Math_String/CHALLENGE/70 StudentToString.java
@@ -1,29 +1,33 @@
 package ControlStatement_Math_String.CHALLENGE;
 
- class StudentToString {
-  String name;
-  int age;
-  String rollNumber;
-  String house;
+class StudentToString {
+ String name;
+ int age;
+ String rollNumber;
+ String house;
 
-  public StudentToString(String name, int age, String rollNumber, String house) {
-   this.name = name;
-   this.age = age;
-   this.rollNumber = rollNumber;
-   this.house = house;
-  }
-
-  @Override
-  public String toString() {
-   return "Student Detail:{name: "+name+
-           ", age: "+age+
-           ", roll number: " +rollNumber+
-           ",house: "+house+" }";
-  }
-
-  public static void main(String[] args) {
-   StudentToString stu = new StudentToString("Ankit",18,
-                                              "246003","Bihar");
-   System.out.println(stu);
-  }
+ // Constructor to initialize student details
+ public StudentToString(String name, int age, String rollNumber, String house) {
+  this.name = name;
+  this.age = age;
+  this.rollNumber = rollNumber;
+  this.house = house;
  }
+
+ // Overriding toString() method to print student details
+ @Override
+ public String toString() {
+  return "Student Details: { " +
+          "Name: " + this.name + ", " +
+          "Age: " + this.age + ", " +
+          "Roll Number: " + this.rollNumber + ", " +
+          "House: " + this.house +
+          " }";
+ }
+
+ // Main method to test the class
+ public static void main(String[] args) {
+  StudentToString stu = new StudentToString("Ankit", 18, "246003", "Bihar");
+  System.out.println(stu);  // Calls toString() automatically
+ }
+}
