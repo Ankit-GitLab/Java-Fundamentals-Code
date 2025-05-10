6f170b4e715553f6cfcc55a3d2d916fccde30fd9
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-10 22:04:09 +0530

    Source Code

--- src/ControlStatement_Math_String/CHALLENGE/75 ArrayToString.java
+++ src/ControlStatement_Math_String/CHALLENGE/75 ArrayToString.java
@@ -3,5 +3,13 @@ package ControlStatement_Math_String.CHALLENGE;
 class ArrayToString {
     public static void main(String[] args) {
         String[] arr = new String[] {"I", "study", "the", "core", "of", "java"};
+
+        StringBuilder sb = new StringBuilder(); // Creates a new StringBuilder object
+
+        for (String str : arr) {
+            sb.append(str).append(" "); // Appends each word followed by a space
+        }
+
+        System.out.println(sb); // Prints the sentence with an extra space at the end
     }
 }
