c32efa7d182515bf857dbf06a4a38c02a78aa374
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-14 10:59:55 +0530

    Source Code

---
+++ src/OOPs/GetterAndSetter/Car.java
@@ -0,0 +1,28 @@
+package OOPs.GetterAndSetter;
+
+// Class representing a Car with some basic properties
+public class Car {
+    // Private fields for encapsulation
+    private String color;           // Color of the car
+    private String model;           // Model name of the car
+    private double fuelLevel;       // Current fuel level in the car
+    private long costOfPurchase;    // Purchase cost of the car
+
+    // Constructor to initialize all the fields
+    public Car(String color, String model, double fuelLevel, long costOfPurchase) {
+        this.color = color;                     // Assign color to the car
+        this.model = model;                     // Assign model name to the car
+        this.fuelLevel = fuelLevel;             // Set initial fuel level
+        this.costOfPurchase = costOfPurchase;   // Set the cost of purchase
+    }
+
+    // Getter method to return the color of the car
+    public String getColor(){
+        return color;
+    }
+
+    // Getter method to return the model of the car
+    public String getModel(){
+        return model;
+    }
+}
