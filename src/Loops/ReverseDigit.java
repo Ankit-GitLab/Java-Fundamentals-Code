350d6b9e269055662db1eda78e3dff258ba2b5ce
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-18 10:15:32 +0530

    Source Code

--- src/Loops/ReverseDigit.java
+++ src/Loops/ReverseDigit.java
@@ -6,14 +6,15 @@ public class ReverseDigit {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your digit to calculate the Reverse : ");
         int num = sc.nextInt();
-        int Reverse = 0;
+        int reverse = 0;
         int sum = num;
         while(num != 0){
-            Reverse = Reverse * 10;
-            Reverse = Reverse+(num % 10);
+            reverse = reverse * 10;
+            reverse = reverse+(num % 10);
             num = num / 10;
         }
-        int result = sum + Reverse;
-        System.out.print("Sum of Num + Resvers is  : " + result);
+        int result = sum + reverse;
+        System.out.println("Reverse number is: "+reverse);
+        System.out.print("Sum of Num + Reverse is  : " + result);
     }
 }
