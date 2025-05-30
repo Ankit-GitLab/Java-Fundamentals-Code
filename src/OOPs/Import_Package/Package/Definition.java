a429b8477c13a5bb984b1459d072ef86203c8745
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-05-30 10:22:03 +0530

    OPP Code Will Be Add

---
+++ src/OOPs/Import_Package/Package/Definition.java
@@ -0,0 +1,19 @@
+package OOPs.Import_Package.Package;
+
+/**
+ * 📦 Package Definition:
+ * Package Java mein ek group hota hai related classes aur interfaces ka,
+ * jo ek saath ek name (namespace) ke andar organize kiya jata hai.
+ *
+ * ✅ Ye code ko:
+ * 1. Saaf-suthra (well-organized) banata hai
+ * 2. Name conflicts (duplicate class names) se bachata hai
+ *
+ * Example: Jaise folders mein files rakhte ho, waise hi Java mein
+ * related classes ko ek package ke andar rakhte hain.
+ */
+public class Definition {
+    public static void main(String[] args) {
+        System.out.println("Welcome (🙏) to Package Definition");
+    }
+}
