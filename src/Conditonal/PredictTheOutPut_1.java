7663e0bb34b2ce1adb355f47d962a3b33a135fe7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-01 09:18:53 +0530

    Delete Code

--- src/Conditonal/PredictTheOutPut_1.java
+++
@@ -1,9 +0,0 @@
-package Conditonal;
-
-public class PredictTheOutPut_1 {
-    public static void main(String[] args) {
-        int x = 10, y = 20;
-        if(x == y);
-        System.out.println(x+""+y);
-    }
-}
