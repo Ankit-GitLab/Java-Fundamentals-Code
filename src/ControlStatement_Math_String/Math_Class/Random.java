7f3935353fadbf7f34978d2c9743d03b57a392ff
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-01 09:19:26 +0530

    Delete Code

--- src/ControlStatement_Math_String/Math_Class/Random.java
+++
@@ -1,8 +0,0 @@
-package ControlStatement_Math_String.Math_Class;
-
-public class Random {
-    public static void main(String[] args) {
-        int randomInt = (int)(Math.random() * 100) + 1; // range 1 to 100
-        System.out.println("Random integer between 1 and 100: " + randomInt);
-    }
-}
