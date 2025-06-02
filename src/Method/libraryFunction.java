52956d1c50499d7d9f2069e93071b55975ea61f5
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-03 01:02:12 +0530

    qwert

---
+++ src/Method/libraryFunction.java
@@ -0,0 +1,27 @@
+package Method;
+
+public class libraryFunction {
+    public static void main(String[] args) {
+        System.out.println("power : "+Math.pow(8,2));
+        System.out.println("Square root : "+Math.sqrt(16));
+        System.out.println("Cube root : "+Math.cbrt(8));
+        System.out.println("absolute value : "+Math.abs(-2));
+        System.out.println("Rounds down to the nearest interger : "+Math.floor(3.84));
+        System.out.println("Rounds up to the nearest interger  : "+Math.ceil(3.84));
+        System.out.println("Max number : "+Math.max(4,6));
+        System.out.println("Min number : "+Math.min(4,6));
+        System.out.println("Max three number : "+Math.max(9,Math.max(5,6)));
+        System.out.println("Max four number : "+Math.max(4,Math.max(5,Math.max(6,9))));
+        System.out.println("Min four number : "+Math.min(4,Math.min(5,Math.min(6,9))));
+
+
+
+
+
+
+
+
+
+
+    }
+}
