71a8e14c3e488fc38227fc8f43b01b62eca59666
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-03 01:02:44 +0530

    sdfghjk

---
+++ src/OOPs/OOps_Principle/Polymorphism.java
@@ -0,0 +1,25 @@
+package OOPs.OOps_Principle;
+
+/**
+ * 🔁 Polymorphism -->
+ * 📘 Meaning: Jab ek method ya function alag-alag forms mein behave karta hai.
+ *             Same method name ho sakta hai, lekin kaam (behavior) alag ho sakta hai.
+ *
+ * ✅ Real Life Example:
+ * Socho ek aadmi:
+ * - Ghar mein beta
+ * - College mein student
+ * - Office mein employee
+ *
+ * 👉 Ek hi aadmi, lekin alag-alag roles — isse hi kehte hain polymorphism.
+ *
+ * 🔄 Java mein polymorphism 2 tareekon se hota hai:
+ * 1. Compile-time (Method Overloading)
+ * 2. Runtime (Method Overriding)
+ */
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        System.out.println("Welcome (🙏) to Polymorphism..");
+    }
+}
