728a841bdf6f9e9b78c99ba153f4ac3f9dc1bd7b
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-03 00:54:41 +0530

    Class

--- src/Class_And_Object/Main.java
+++
@@ -1,7 +0,0 @@
-package Class_And_Object;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My First Program In Class & Object \n Jay Shri Ram");
-    }
-}
