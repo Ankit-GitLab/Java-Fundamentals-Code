1fc5da44969fb720ae4b1cc43315fe774f1255f1
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-04 01:14:10 +0530

    Add

---
+++ src/Conditonal/PredictTheOutput_2.java
@@ -0,0 +1,11 @@
+package Conditonal;
+
+public class PredictTheOutput_2 {
+    public static void main(String[] args) {
+        int a = 300,b,c;
+        if(a>=400)
+            b = 300;
+        c = 200;
+        System.out.println(a+""+c);
+    }
+}
