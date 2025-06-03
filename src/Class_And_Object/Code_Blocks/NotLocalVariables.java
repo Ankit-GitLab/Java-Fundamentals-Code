dd30d3c9d3bc55a6838ef74a914f58bd5383ae1d
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-04 01:13:36 +0530

    Add

---
+++ src/Class_And_Object/Code_Blocks/NotLocalVariables.java
@@ -0,0 +1,8 @@
+package Class_And_Object.Code_Blocks;
+
+public class NotLocalVariables {
+    public void display() {
+        int x; // not initialized
+        // System.out.println(x); //  Compile-time error
+    }
+}
