d0aaf061634fed6118449f76d2e032e9936af893
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-06 19:37:02 +0530

    Add

---
+++ src/Loops/ASCIIValue.java
@@ -0,0 +1,8 @@
+package Loops;
+public class ASCIIValue {
+    public static void main(String[] args) {
+        for(int i=65; i<=90; i++){
+            System.out.println(i+" ---> "+(char)i);
+        }
+    }
+}
