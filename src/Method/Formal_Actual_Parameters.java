01eac8039c8190c9eced0a43589661b3284e378a
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-07 07:48:00 +0530

    Add

---
+++ src/Method/Formal_Actual_Parameters.java
@@ -0,0 +1,26 @@
+package Method;
+
+import java.util.Scanner;
+
+public class Formal_Actual_Parameters {
+    public static void swap(int a, int b){ // this one is formal parameter
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swap --> "+a+"  "+b);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b number : ");
+        int b = sc.nextInt();
+        System.out.println("Before Swap --> "+a+"  "+b);
+        swap(a,b); // this one is actual parameter
+        System.out.println("Before Swap --> "+a+"  "+b);
+
+        sc.close();
+
+    }
+}
