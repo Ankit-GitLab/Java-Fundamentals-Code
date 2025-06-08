c30e2791817d14ba4423f8eb6c7f9b22cc07e8cf
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-08 10:29:24 +0530

    Add

---
+++ src/BasicsOfJava/IncrementDecrement.java
@@ -0,0 +1,15 @@
+package BasicsOfJava;
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 5;
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+        ++x; 
+        System.out.println(x);
+        --x;
+        System.out.println(x);
+    }
+    
+}
