479d49a229d3263697841379431c27afa771d503
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-08 10:37:05 +0530

    Add

---
+++ src/OOPs/Import_Package/Import/TypesOfImport.java
@@ -0,0 +1,15 @@
+package OOPs.Import_Package.Import; // Package declaration
+
+/**
+ * 🔄 **Types of Import:**
+ *  ✅ If you want to import a *specific class*, write the class name.
+ *     ➤ Example: import java.util.Scanner;
+ *
+ *  ✅ If you want to import *all classes* from a package, use `*`.
+ *     ➤ Example: import java.util.*;
+ */
+public class TypesOfImport {
+    public static void main(String[] args) {
+        System.out.println("Welcome (🙏) to types of Import...");
+    }
+}
