695ebea110a3d68b2594511835555b560b69091d
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-09 00:42:44 +0530

    Add the code

---
+++ src/Class_And_Object/Primitive_NonPrimitive/Primitive.java
@@ -0,0 +1,17 @@
+package Class_And_Object.Primitive_NonPrimitive;
+
+public class Primitive {
+
+    public static void main(String[] args) {
+        int age = 20;           // integer value
+        float marks = 85.5f;    // floating point number
+        char grade = 'A';       // character
+        boolean passed = true;  // boolean value
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
+
