03514c23a34b04840ff4abde11d21115577396c6
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-08 10:30:29 +0530

    Add

---
+++ src/Loops/LoopBasics.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class LoopBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number how many time you want to print Ankit kumar : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.println("Ankit Kumar");
+        }
+        System.out.println();
+        sc.close();
+
+    }
+}
