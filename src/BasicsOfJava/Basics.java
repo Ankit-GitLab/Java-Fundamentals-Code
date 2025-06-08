78dfff0848f7a13971f7147fdf7ad14baa3c06be
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-09 00:33:27 +0530

    Delete

--- src/BasicsOfJava/Basics.java
+++
@@ -1,8 +0,0 @@
-package BasicsOfJava;
-
-public class Basics {
-    public static void main(String[] args) {
-        System.out.println("Hello world My First programming in java Ankit" + "   sasasasasasasadfgrhjkl");
-
-    }
-}
