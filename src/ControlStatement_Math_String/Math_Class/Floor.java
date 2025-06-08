4e2a02fee091c1e39decbec2932cc0cb51c9f945
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-08 10:35:12 +0530

    Add

---
+++ src/ControlStatement_Math_String/Math_Class/Floor.java
@@ -0,0 +1,12 @@
+package ControlStatement_Math_String.Math_Class;
+
+public class Floor {
+    public static void main(String[] args) {
+        double a = 5.9;
+        double b = -4.2;
+
+        // Using Math.floor()
+        System.out.println("Floor of " + a + " is: " + Math.floor(a)); // Output: 5.0
+        System.out.println("Floor of " + b + " is: " + Math.floor(b)); // Output: -5.0
+    }
+}
