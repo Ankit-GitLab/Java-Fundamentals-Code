b160040ca7168cdbfe1604bc541ddf00345f61b3
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-10 00:36:02 +0530

    Delete

--- src/BasicsOfJava/VolumeOfASphere.java
+++
@@ -1,9 +0,0 @@
-package BasicsOfJava;
-public class VolumeOfASphere {
-    public static void main(String[] args) {
-        double radius = 5;
-        double PI = 3.14;
-        double area = (4/3) * PI*radius*radius*radius;
-        System.out.println(area);
-    }
-}
