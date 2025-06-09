e4717f8a91e7ccd2b6c4ea5ccadbca8ba39950c1
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-10 00:44:40 +0530

    Add one object question

---
+++ src/Class_And_Object/Main.java
@@ -0,0 +1,7 @@
+package Class_And_Object;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My First Program In Class & Object \n Jay Shri Ram");
+    }
+}
