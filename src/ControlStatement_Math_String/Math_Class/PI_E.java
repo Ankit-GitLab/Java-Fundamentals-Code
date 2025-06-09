ac86ad995bebb364dd0719a0b7e1dd26a93d266f
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-10 00:37:24 +0530

    Delete

--- src/ControlStatement_Math_String/Math_Class/PI_E.java
+++
@@ -1,9 +0,0 @@
-package ControlStatement_Math_String.Math_Class;
-
-public class PI_E {
-    public static void main(String[] args) {
-        // Accessing Math.PI and Math.E
-        System.out.println("Value of Pi: " + Math.PI);
-        System.out.println("Value of E: " + Math.E);
-    }
-}
