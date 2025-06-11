98e60466fab278ef39f642372cedb5a63822f201
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-11 07:19:09 +0530

    Ankit fix

---
+++ src/Conditonal/PredictTheOutput_3.java
@@ -0,0 +1,9 @@
+package Conditonal;
+
+public class PredictTheOutput_3 {
+    public static void main(String[] args) {
+        int x = 10;
+        boolean z = x < 10;
+        System.out.println(z);
+    }
+}
