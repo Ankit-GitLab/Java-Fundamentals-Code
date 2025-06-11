80ae10f601a3b35ccc8fbfbfda4d677c36c4e0d3
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-11 07:17:15 +0530

    Add the question

---
+++ src/Class_And_Object/Constructor/Default.java
@@ -0,0 +1,12 @@
+package Class_And_Object.Constructor;
+
+class Default {
+    // Default constructor
+    Default() {
+        System.out.println("Car object created.");
+    }
+
+    public static void main(String[] args) {
+        Default myCar = new Default();  // Constructor is called here
+    }
+}
