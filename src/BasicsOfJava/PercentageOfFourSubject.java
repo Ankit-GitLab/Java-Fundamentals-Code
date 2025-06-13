c4a9761a069b63f444873f614668584bbb88d141
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-14 04:33:10 +0530

    Add

---
+++ src/BasicsOfJava/PercentageOfFourSubject.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+public class PercentageOfFourSubject {
+    public static void main(String[] args) {
+        double m1 = 40;
+        double m2 = 50;
+        double m3 = 60;
+        double m4 = 60;
+        double y = ((m1 + m2 + m3 + m4) / 240) * 100;
+        System.out.println(y);
+    }
+}
